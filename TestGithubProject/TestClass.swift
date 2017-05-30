1f42bbb10f521b789e2aa6036a21fc7c6385f2a8
Terry <sassyforever@gmail.com>
2017-05-30 17:10:18 +0800

    add a new file

---
+++ TestGithubProject/TestClass.swift
@@ -0,0 +1,13 @@
+//
+//  TestaClass.swift
+//  TestGithubProject
+//
+//  Created by  Terry on 2017/5/30.
+//  Copyright © 2017年 Terry. All rights reserved.
+//
+
+import UIKit
+
+class TestClass: NSObject {
+
+}
